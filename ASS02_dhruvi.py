a15e4f41c75ae9cef801b074287c8579e466a661
Dhruvi bansal <48863167+Dhruvibansal@users.noreply.github.com>
2020-05-28 01:26:47 +0530

    Add files via upload

---
+++ ASS02_dhruvi.py
@@ -0,0 +1,5 @@
+q1 = int(input("Enter the first charge value: "))
+q2 = int(input("Enter the second charge value: "))
+r  = int(input("Enter the distance between both charges:"))
+Force  =   9*10**9*q1*q2/r*r
+print("Force is:", abs(Force)) 
